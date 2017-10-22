c31b0c3fd5cf5912713f53fefb6093a04b43fec5
ThomasVB <van.bouwel.thomas@gmail.com>
2017-10-22 14:11:43 -0400

    correction in translation method + namechanges

--- Assets/#Project/scripts/WorkspaceMovement.cs
+++ Assets/#Project/scripts/WorkspaceMovement.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class ModelMovement : MonoBehaviour {
+public class WorkspaceMovement : MonoBehaviour {
 
     public VRTK_ControllerEvents leftController;
     public VRTK_ControllerEvents rightController;
-    public Transform model;
+    public Transform workspace;
 
     private Vector3 leftPos;
     private Vector3 rightPos;
@@ -42,45 +42,52 @@ public class ModelMovement : MonoBehaviour {
     }
 
     private void TwoHandDrag() {
-        Vector3 pos = (leftController.transform.position + rightController.transform.position) / 2f;
+        //get middle position of hands
+        Vector3 pos = (leftPos + rightPos) / 2f;
         Vector3 prevPos = (leftPosPrev + rightPosPrev) / 2f;
         Translate(pos, prevPos);
     }
 
     private void Translate(Vector3 pos, Vector3 prevPos) {
         Vector3 distance = pos - prevPos;
-        model.Translate(distance);
+        workspace.Translate(distance, Space.World);
     }
 
     private void Rotate() {
         //project on XZ plane to restric rotation to flat table
-        Vector3 pos = Vector3.ProjectOnPlane(rightPos - leftPos, Vector3.up);
-        Vector3 prevPos = Vector3.ProjectOnPlane(rightPosPrev - leftPosPrev, Vector3.up);
+        Vector3 dir = Vector3.ProjectOnPlane(rightPos - leftPos, Vector3.up);
+        Vector3 prevDir = Vector3.ProjectOnPlane(rightPosPrev - leftPosPrev, Vector3.up);
 
-        //center of first hand pos
+        //center of hand pos
         Vector3 center = (leftPos + rightPos) / 2f;
 
         //rotate around center 
-        float angle = Vector3.Angle(pos, prevPos);
-        Vector3 cross = Vector3.Cross(pos, prevPos);
+        float angle = Vector3.Angle(dir, prevDir);
+        Vector3 cross = Vector3.Cross(dir, prevDir);
         float dot = Vector3.Dot(cross, Vector3.down);
         float sign = Mathf.Sign(dot);
 
         //perform rotation
-        model.RotateAround(center, Vector3.up, sign * angle);
+        workspace.RotateAround(center, Vector3.up, sign * angle);
     }
 
     private void Scale() {
+        //distance between hands 
         float dist = Vector3.Distance(leftPos, rightPos);
         float prevDist = Vector3.Distance(leftPosPrev, rightPosPrev);
+
+        //scale factor based on difference in hand distance
         float scaleFactor = dist / prevDist;
 
+        //convert middle position of hands from global to local space
         Vector3 midPosPreScale = (rightPos + leftPos) / 2f;
-        Vector3 midPosLocal = model.InverseTransformPoint(midPosPreScale);
+        Vector3 midPosLocal = workspace.InverseTransformPoint(midPosPreScale);
 
-        model.localScale *= scaleFactor;
+        //apply scale to model
+        workspace.localScale *= scaleFactor;
 
-        Vector3 midPosPostScale = model.TransformPoint(midPosLocal);
+        //convert local position back to global and perform corrective translation
+        Vector3 midPosPostScale = workspace.TransformPoint(midPosLocal);
         Translate(midPosPreScale, midPosPostScale);
     }
 
