d727ce61dae3f0b42f0106eb61d83f6ab7927367
ThomasVB <van.bouwel.thomas@gmail.com>
2017-10-24 22:16:10 -0400

    example scenes

---
+++ Assets/#Project/scripts/Examples/Drawtool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Drawtool : MonoBehaviour {
+
+    public Transform parent;
+    public Transform drawTip;
+    public VRTK_ControllerEvents rightController;
+    public float lineWidth = .01f;
+    public Material lineMaterial;
+
+    void Start() {
+        rightController.TriggerPressed += StartDrawing;
+        rightController.TriggerReleased += StopDrawing;
+    }
+
+    private void StartDrawing(object sender, ControllerInteractionEventArgs e) {
+        LineRenderer line = CreateNewLine();
+        StartCoroutine(AddPoints(line));
+    }
+
+    private void StopDrawing(object sender, ControllerInteractionEventArgs e) {
+        StopAllCoroutines();
+    }
+
+    private LineRenderer CreateNewLine() {
+        GameObject obj = new GameObject();
+        obj.transform.parent = parent;
+
+        LineRenderer lr = obj.AddComponent<LineRenderer>();
+        lr.startWidth = lineWidth;
+        lr.endWidth = lineWidth;
+        lr.material = lineMaterial;
+        lr.useWorldSpace = false;
+
+        return lr;
+    }
+
+    IEnumerator AddPoints(LineRenderer line) {
+        line.positionCount = 0;
+
+        while (true) {
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, drawTip.position);
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/#Project/scripts/Examples/PrefabSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PrefabSpawner : MonoBehaviour {
+
+    public Transform parent;
+    public GameObject prefab;
+    public VRTK_ControllerEvents controller;
+
+    void Start () {
+        controller.TriggerPressed += StartSpawning;
+    }
+
+    private void StartSpawning(object sender, ControllerInteractionEventArgs e) {
+        StartCoroutine(SpawnLoop());
+    }
+
+    IEnumerator SpawnLoop() {
+        GameObject go = Instantiate(prefab);
+        go.transform.parent = parent;
+        go.transform.localScale = Vector3.one;
+
+        while (controller.triggerPressed) {
+            RaycastHit hit;
+            if (Physics.Raycast(controller.transform.position, controller.transform.forward, out hit)) {
+                go.transform.position = hit.point;
+            }
+
+            yield return null;
+        }
+    }
+}
